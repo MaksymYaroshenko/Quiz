125bac6e2a75eb962e06927366a1bb0787148024
Maksym Yaroshenko <yarrok98@gmail.com>
2022-03-20 16:50:59 +0200

    update UI on the Login page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
